220d7e18748bbd694af99f449c32c99a3ef66a6a
Sushovan Saha <sushovansaha95@gmail.com>
2023-03-25 23:25:31 +0530

    Upto Navbar

---
+++ FoodDeliveryApp/mernapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './screens/Home';
+
+
+function App() {
+  return (
+    <>
+      <div className='fs-1'> {/* font for bootstarp */}
+        <Home></Home>
+      </div>
+    </>    
+  );
+}
+
+export default App;
